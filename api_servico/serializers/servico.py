cf0c3be5f0dda15eb4d7d49d3602e54c3ec1993e
Robson Pastor Alexandre <robsonpastor@gmail.com>
2017-02-03 22:24:00 -0300

    Refactoring Serviço

--- api_servico/serializers/servico.py
+++ api_servico/serializers/servico.py
@@ -11,35 +11,32 @@ class ServicoSerializer(MyApiSerializer):
         model = Servico
         fields = ('id','nome', 'data_inicio', 'data_fim', 'safra', 'servico_itens', 'custo_total')
         extra_kwargs = {'custo_total':{'read_only':True}}
-    def update(self, instance, validated_data):
-        valid_fields = self.writable_fields()
+
+    def save_servico_itens(self, servico, servico_itens_data):
         ids = []
-        for attr  in valid_fields:
-            setattr(instance, attr, validated_data[attr])
-        instance.save()
         custo_total = 0
-        for item_data in validated_data['servico_itens']:
-            servico_item = self.fill_save_model(ServicoItem( servico=instance),item_data)
-            custo_total+=servico_item.custo_item
+        for item_data in servico_itens_data:
+            servico_item = self.fill_save_model(ServicoItem(servico=servico), item_data)
+            custo_total += servico_item.custo_item
             ids.append(servico_item.id)
         
-        if instance.custo_total != custo_total:
-            instance.custo_total = custo_total
-            instance.save()
+        if servico.custo_total != custo_total:
+            servico.custo_total = custo_total
+            servico.save()
+        return ids
+
+    def update(self, instance, validated_data):
+        servico_itens_data = validated_data.pop('servico_itens', [])
+        instance = super(ServicoSerializer, self).update(instance,validated_data)
+        
+        ids = self.save_servico_itens(instance, servico_itens_data)
         ServicoItem.objects.filter(servico=instance).exclude(id__in=ids).delete()
         return instance
         
     def create(self, validated_data):
         servico_itens_data = validated_data.pop('servico_itens', [])
         instance = super(ServicoSerializer, self).create(validated_data)
-        servico_itens = []
-        custo_total = 0
-        for item_data in servico_itens_data:
-            servico_item = self.fill_save_model(ServicoItem( servico=instance),item_data)
-            servico_itens.append( servico_item)
-            custo_total+=servico_item.custo_item
-        if instance.custo_total != custo_total:
-            instance.custo_total = custo_total
-            instance.save()
-        instance.servico_itens = servico_itens
+        
+        self.save_servico_itens(instance, servico_itens_data)
+        
         return instance
