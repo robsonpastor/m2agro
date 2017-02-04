13c185a47e13afc8d0c2190fefa8a1695811677d
Robson Pastor Alexandre <robsonpastor@gmail.com>
2017-02-04 14:55:51 -0300

    Restruturação dos campos da classe abstrata

--- m2agro/utils/models.py
+++ m2agro/utils/models.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.utils.timezone import utc
-import datetime
 from django.db import models
-
-
 class MyApiModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True,default=datetime.datetime(2016, 9, 9, 11, 15, 15, 666614, tzinfo=utc))
-    updated_at = models.DateTimeField(auto_now=True,default=datetime.datetime(2016, 9, 9, 11, 15, 15, 666614, tzinfo=utc))
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
-
+    
+    def save(self, *args, **kw):
+        if self.pk is not None and self.created_at is None:
+            original = self.__class__.objects.get(pk=self.pk)
+            self.created_at = original.created_at
+        super(MyApiModel, self).save( *args, **kw)
